e2aaaafccf551539762d1e23ab784a03539fe0d4
MahboubehSeyedpour <mahboobeh.seyedpour@gmail.com>
2021-04-09 21:56:58 +0430

    set the toolbar as an app bar for activity

--- app/src/main/java/com/example/cassette/views/MainActivity.kt
+++ app/src/main/java/com/example/cassette/views/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.cassette.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.cassette.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar)
+
+//        TODO( "implement hideStatusBar() function");
+
     }
 }
