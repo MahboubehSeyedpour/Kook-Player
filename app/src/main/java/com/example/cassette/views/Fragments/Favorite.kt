09657553c65a3861ea6965cbdcd7773c6de48f71
MahboubehSeyedpour <mahboobeh.seyedpour@gmail.com>
2021-05-11 06:16:41 +0430

    add sample fragments for tabs

---
+++ app/src/main/java/com/example/cassette/views/Fragments/Favorite.kt
@@ -0,0 +1,18 @@
+package com.example.cassette.views.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cassette.R
+
+class Favorite : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.favorite_fragment, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/cassette/views/Fragments/Library.kt
@@ -0,0 +1,18 @@
+package com.example.cassette.views.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cassette.R
+
+class Library : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.library_fragment, container, false)
+    }
+}
