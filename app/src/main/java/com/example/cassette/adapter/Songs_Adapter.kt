dc887e230d9b1ace0a48a6ad4eec69a9f6154721
MahboubehSeyedpour <mahboobeh.seyedpour@gmail.com>
2021-05-27 17:18:07 +0430

    add an onclickListener to RecyclerView items

--- app/src/main/java/com/example/cassette/adapter/Songs_Adapter.kt
+++ app/src/main/java/com/example/cassette/adapter/Songs_Adapter.kt
@@ -4,13 +4,16 @@ import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cassette.R
 import com.example.cassette.models.Song_Model
 import kotlinx.android.synthetic.main.song_rv_item.view.*
 
-class Songs_Adapter(context: Activity,
-                    arrayList: ArrayList<Song_Model>
+class Songs_Adapter(
+    context: Activity,
+    arrayList: ArrayList<Song_Model>
 ) :
     RV_Base_Adapter() {
 
@@ -30,10 +33,17 @@ class Songs_Adapter(context: Activity,
         holder: RecyclerView.ViewHolder,
         position: Int
     ) {
-        val song : Song_Model = arrayList.get(position)
+        val song: Song_Model = arrayList.get(position)
         val viewHolder = holder as RecyclerViewViewHolder
         viewHolder.title.text = song.title
         viewHolder.duration.text = song.duration
+        viewHolder.itemView.setOnClickListener{
+            Toast.makeText(
+                context,
+                song.title,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun getItemCount(): Int {
