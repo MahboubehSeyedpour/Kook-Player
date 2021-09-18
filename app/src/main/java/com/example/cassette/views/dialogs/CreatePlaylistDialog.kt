095496a5f607e47b21b2c494b295fe203bc14f02
MahboubehSeyedpour <mahboobeh.seyedpour@gmail.com>
2021-09-18 04:54:11 +0430

    check that the name is not empty and unique for the playlist + dismiss the dialog when the cancel button is clicked

--- app/src/main/java/com/example/cassette/views/dialogs/CreatePlaylistDialog.kt
+++ app/src/main/java/com/example/cassette/views/dialogs/CreatePlaylistDialog.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.cassette.R
 import com.example.cassette.databinding.CreatePlaylistDialogBinding
 import com.example.cassette.myInterface.PassData
+import com.example.cassette.views.Fragments.PlaylistFragment
+import com.example.cassette.views.Fragments.PlaylistPageFragment
+import com.example.cassette.views.MainActivity
 import kotlinx.android.synthetic.main.create_playlist_dialog.*
 import kotlinx.android.synthetic.main.create_playlist_dialog.view.*
 
@@ -34,12 +40,48 @@ class CreatePlaylistDialog : DialogFragment() {
 
         btnPositive.setOnClickListener {
 
-            val targetFragment = targetFragment
-            val passData: PassData = targetFragment as PassData
-            targetFragment.passDataToInvokingFragment(binding.textField.playlist_name.text.toString())
+            if(binding.textField.playlist_name.text.toString().isEmpty())
+            {
+                val shake: Animation = AnimationUtils.loadAnimation(this@CreatePlaylistDialog.context, R.anim.shake)
+                binding.addPlaylistLayout.startAnimation(shake)
+                Toast.makeText(
+                    context,
+                    "Please enter a name",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else if(isUnique(binding.textField.playlist_name.text.toString()))
+            {
+                val shake: Animation = AnimationUtils.loadAnimation(this@CreatePlaylistDialog.context, R.anim.shake)
+                binding.addPlaylistLayout.startAnimation(shake)
+                Toast.makeText(
+                    context,
+                    "Duplicate name",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else
+            {
+                val targetFragment = targetFragment
+                val passData: PassData = targetFragment as PassData
+                targetFragment.passDataToInvokingFragment(binding.textField.playlist_name.text.toString())
 
-            this.dismiss()
-
+                this.dismiss()
+            }
         }
+
+        btnNegative.setOnClickListener {
+            this.dismiss()
+        }
+    }
+
+    private fun isUnique(name: String): Boolean
+    {
+        for (playlist in PlaylistFragment.viewModel?.playlistRepository?.cashedPlaylistArray!!)
+        {
+            if(playlist.name == name)
+                return true
+        }
+        return false
     }
 }
